add23d9f9519012012ff57773d86fed79528f23d
David Mercier <david9684@gmail.com>
2015-12-18 20:06:36 +0100

    created util folder with util Matlab scripts

---
+++ matlab_code/util/get_tridim_root.m
@@ -0,0 +1,15 @@
+%% Copyright 2014 MERCIER David
+function tridim_root = get_tridim_root
+%% Get environment variable for NIMS
+
+tridim_root = getenv('TRIDIM_TBX_ROOT');
+
+if isempty(tridim_root)
+    msg = 'Run the path_management.m script !';
+    commandwindow;
+    display(msg);
+    %errordlg(msg, 'File Error');
+    error(msg);
+end
+
+end
