e0053cdb19342b380255557046eba6a24ff2f175
David Mercier <david9684@gmail.com>
2015-12-18 20:06:14 +0100

    moved main Matlab code

---
+++ matlab_code/TriDiMap_loadingData.m
@@ -0,0 +1,10 @@
+%% Copyright 2014 MERCIER David
+function expValues = TriDiMap_loadingData()
+%% Function to load 3D map of elastic/plastic properties from indentation tests
+
+
+
+expValues
+
+
+end
