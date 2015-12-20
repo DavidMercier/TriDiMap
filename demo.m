eef3f9cdc9534c711823bc69285ea2423d0fba0e
David Mercier <david9684@gmail.com>
2015-12-20 21:28:06 +0100

    updated and debugged functions to load data from Excel file and to plot exp. results

--- demo.m
+++ demo.m
@@ -1,5 +1,5 @@
 %% Copyright 2014 MERCIER David
-function demo
+function gui = demo
 %% Function to run the Matlab code for the 3D mapping of indentation data
 
 %% Define gui structure variable
@@ -20,9 +20,46 @@ catch
     gui.config.tridim_root = get_tridim_root; % ensure that environment is set
 end
 
-%% Load data
+gui.config.data.data_path = '.\data_indentation';
 
-%% Run the 3D mapping
-TriDiMap_mapping_plotting;
+%% Set variables
+gui.config.data.normalizationStep = 0; % 0 if no normalization and 1 if normalization step
+gui.config.data.N_XStep = 5; % Number of steps along X axis
+gui.config.data.N_YStep = 10; % Number of steps along Y axis
+gui.config.data.XStep = 15; % X step in microns
+gui.config.data.YStep = 15; % Y step in microns
 
+%% Load data from Excel files
+[config, gui.data] = ...
+    TriDiMap_loadingData(gui.config.data.data_path, ...
+    gui.config.data.N_XStep, gui.config.data.N_YStep);
+
+gui.config.data_path = config.data_path;
+gui.config.flag.flag_data = config.flag.flag_data;
+
+if gui.config.flag.flag_data
+    %% Normalization step
+    normStep = gui.config.data.normalizationStep;
+    if normStep
+        % Normalization of elastic modulus
+        gui.data.expValues_mat_average.YM = mean(mean(gui.data.expValues_mat.YM));
+        gui.data.expValues_mat_norm.YM = gui.data.expValues_mat.YM - gui.data.expValues_mat_average.YM;
+        % Normalization of hardness
+        gui.data.expValues_mat_average.H = mean(mean(gui.data.expValues_mat.H));
+        gui.data.expValues_mat_norm.H = gui.data.expValues_mat.H - gui.data.expValues_mat_average.H;
+    end
+    
+    %% Run the 3D maps
+    if normStep
+        TriDiMap_mapping_plotting(gui.config.data.XStep, gui.config.data.YStep, ...
+            gui.data.expValues_mat_norm.YM, 1, normStep);
+        TriDiMap_mapping_plotting(gui.config.data.XStep, gui.config.data.YStep, ...
+            gui.data.expValues_mat_norm.H, 2, normStep);
+    else
+        TriDiMap_mapping_plotting(gui.config.data.XStep, gui.config.data.YStep, ...
+            gui.data.expValues_mat.YM, 1, normStep);
+        TriDiMap_mapping_plotting(gui.config.data.XStep, gui.config.data.YStep, ...
+            gui.data.expValues_mat.H, 2, normStep);
+    end
+end
 end
--- matlab_code/TriDiMap_loadingData.m
+++ matlab_code/TriDiMap_loadingData.m
@@ -1,10 +1,62 @@
 %% Copyright 2014 MERCIER David
-function expValues = TriDiMap_loadingData()
-%% Function to load 3D map of elastic/plastic properties from indentation tests
+function [config, data] = TriDiMap_loadingData(data_path, NX, NY)
+%% Function to load Excel data from indentation tests
 
+%% Open window to select file
+title_importdata_Window = 'File Selector from';
 
+[data.filename_data, data.pathname_data, filterindex_data] = ...
+    uigetfile('*.xls;*.xlsx', ...
+    char(title_importdata_Window), data_path);
 
-expValues
+if data_path ~= 0
+    config.data_path = data_path;
+end
 
+%% Handle canceled file selection
+if data.filename_data == 0
+    data.filename_data = '';
+end
+if data.pathname_data == 0
+    data.pathname_data = '';
+else
+    config.data_path = data.pathname_data;
+end
 
+if isequal(data.filename_data,'')
+    disp('User selected Cancel');
+    data.pathname_data = 'no_data';
+    data.filename_data = 'no_data';
+    ext = '.nul';
+    config.flag.flag_data = 0;
+else
+    disp(['User selected', ...
+        fullfile(data.pathname_data, data.filename_data)]);
+    [pathstr, name, ext] = fileparts(data.filename_data);
+    config.flag.flag_data = 1;
+end
+
+data2import = [data.pathname_data, data.filename_data];
+
+%% .txt file
+if config.flag.flag_data
+    if strcmp (ext, '.xls') == 1 || strcmp (ext, '.xlsx') == 1
+        [dataAll, txtAll] = xlsread(data2import, 'Results');
+        % First column is text in 'Results' sheet !
+        data.expValues.YM = dataAll(:,1);
+        data.expValues.H = dataAll(:,2);
+    end
+    
+    % Vector to matrix (last three columns are average values in 'Results' sheet
+    data.expValues_mat.YM = reshape(data.expValues.YM(1:end-3,1),[NX,NY]);
+    data.expValues_mat.H = reshape(data.expValues.H(1:end-3,1),[NX,NY]);
+    
+    % Flip even columns to respect nanoindentation pattern
+    for evenIndex = 2:2:NY
+        data.expValues_mat.YM(:,evenIndex) = ...
+            flipud(data.expValues_mat.YM(:,evenIndex));
+        data.expValues_mat.H(:,evenIndex) = ...
+            flipud(data.expValues_mat.H(:,evenIndex));
+    end
+end
 end
