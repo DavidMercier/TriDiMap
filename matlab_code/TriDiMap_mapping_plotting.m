5c6209aa498c8a61963f2f9ca12031a3a39a5387
David Mercier <david9684@gmail.com>
2016-07-03 22:00:15 +0200

    clarified smoothing step and corrected plot of markers

--- matlab_code/TriDiMap_mapping_plotting.m
+++ matlab_code/TriDiMap_mapping_plotting.m
@@ -1,10 +1,10 @@
 %% Copyright 2014 MERCIER David
-function TriDiMap_mapping_plotting(x_step, y_step, Nr, Nc, expValues, expProp, ...
-    normStep, interpolFac, cmin, cmax, scaleAxis, TriDiView, FontSizeVal, ...
+function TriDiMap_mapping_plotting(x_step, y_step, smoothVal, expValues, expProp, ...
+    normStep, interpol, interpolFac, cmin, cmax, scaleAxis, TriDiView, FontSizeVal, ...
     Markers, varargin)
 %% Function to plot a 3D map of elastic/plastic properties in function of X/Y coordinates
 % x_step and y_step : Steps between indents in X and Y axis in microns.
-% Nr and Nc : Number of points used to smooth respectively rows and columns
+% smooth : Number of points used to smooth respectively rows and columns (Nc and Nr)
 % expValues : Values of experimental properties obtained by indentation in GPa.
 % expProp : 1 = Elastic (Young's modulus) or 2 = Plastic (hardness) properties
 % normStep : Boolean to set step of normalization (0 = no normalization and 1 = normalization)
@@ -45,24 +45,24 @@ if nargin < 8
 end
 
 if nargin < 7
-    normStep = 0;
+    interpol = 1;
 end
 
 if nargin < 6
-    expProp = 1;
+    normStep = 0;
 end
 
 if nargin < 5
+    expProp = 1;
+end
+
+if nargin < 4
     %expValues = randi(101,101);
     expValues = peaks(51);
 end
 
-if nargin < 4
-    Nc = 2;
-end
-
 if nargin < 3
-    Nr = 2;
+    smoothVal = 2;
 end
 
 if nargin < 2
@@ -74,17 +74,35 @@ if nargin < 1
 end
 
 %% Interpolation - Definition of pixels coordinates
+if ~interpol
+    interpolFac = 0;
+end
 expValues_interp = interp2(expValues,interpolFac);
+maxValInterpol = max(max(expValues_interp));
+minValInterpol = min(min(expValues_interp));
 
 %% Data smoothing
-expValues_interp = smooth2a(expValues_interp, Nr, Nc);
+expValues_interpSmoothed = smooth2a(expValues_interp, smoothVal, smoothVal);
+maxValSmoothed = max(max(expValues_interpSmoothed));
+minValSmoothed = min(min(expValues_interpSmoothed));
+
+%% Correction of loss after smoothing step
+ratioMax = (maxValInterpol/maxValSmoothed);
+ratioMin = (minValInterpol/minValSmoothed);
+ratioMean = mean([ratioMin, ratioMax]);
+%expValues_interp = expValues_interpSmoothed*ratioMean;
+expValues_interp = expValues_interpSmoothed;
 
 %% Grid meshing
+[xData_markers, yData_markers] = ...
+    meshgrid(0:x_step:(size(expValues,1)-1)*x_step, ...
+    0:y_step:(size(expValues,2)-1)*y_step);
+
 [xData_interp, yData_interp] = ...
     meshgrid(0:x_step:(size(expValues_interp,1)-1)*x_step, ...
     0:y_step:(size(expValues_interp,2)-1)*y_step);
 
-if interpolFac
+if interpol
     xData_interp = xData_interp./(2^(interpolFac));
     yData_interp = yData_interp./(2^(interpolFac));
 end
@@ -110,7 +128,7 @@ elseif normStep
     if expProp == 1
         zString = 'Normalized elastic modulus';
     elseif expProp == 2
-        zString = 'Normalized Hardness';
+        zString = 'Normalized hardness';
     end
 end
 
@@ -229,7 +247,7 @@ else
     hold on;
     
     if Markers
-        plot3(xData_interp, yData_interp, expValues_interp','k+');
+        plot3(xData_markers, yData_markers, expValues','k+');
         hold on;
     end
     
