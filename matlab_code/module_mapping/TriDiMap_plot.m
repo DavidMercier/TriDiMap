9aef6fb84f1668df42e35f63a9b7c408f150b0b0
David Mercier <david9684@gmail.com>
2024-09-27 15:12:48 +0200

    Debugged binarization module

--- matlab_code/module_mapping/TriDiMap_plot.m
+++ matlab_code/module_mapping/TriDiMap_plot.m
@@ -8,20 +8,23 @@ if rawData < 8
     xData_interp = gui.data.xData_interp;
     yData_interp = gui.data.yData_interp;
     data2plot = gui.data.data2plot;
-    if get(gui.handles.pm_autoColorbar_GUI, 'Value') == 3 && str2num(get(gui.handles.value_PhNumHist_GUI, 'String'))>1
-        if gui.config.property == 1 && isfield(gui.resultsPDF, 'E')
-            resultsPDF = gui.resultsPDF.E;
-        elseif gui.config.property == 2 && isfield(gui.resultsPDF, 'H')
-            resultsPDF = gui.resultsPDF.H;
-        else
-            resultsPDF.transitionVal= 0;
-            disp('Run first deconvolution process of the probability density function');
+    try
+        if get(gui.handles.pm_autoColorbar_GUI, 'Value') == 3 && str2num(get(gui.handles.value_PhNumHist_GUI, 'String'))>1
+            if gui.config.property == 1 && isfield(gui.resultsPDF, 'E')
+                resultsPDF = gui.resultsPDF.E;
+            elseif gui.config.property == 2 && isfield(gui.resultsPDF, 'H')
+                resultsPDF = gui.resultsPDF.H;
+            else
+                resultsPDF.transitionVal= 0;
+                disp('Run first deconvolution process of the probability density function');
+            end
+            data2plot(data2plot < resultsPDF.transitionVal(1)) = 1;
+            for ii = 2:(resultsPDF.M)-1
+                data2plot(data2plot > resultsPDF.transitionVal(ii-1) & data2plot < resultsPDF.transitionVal(ii)) = ii;
+            end
+            data2plot(data2plot > resultsPDF.transitionVal(end)) = str2num(get(gui.handles.value_PhNumHist_GUI, 'String'));
         end
-        data2plot(data2plot < resultsPDF.transitionVal(1)) = 1;
-        for ii = 2:(resultsPDF.M)-1
-            data2plot(data2plot > resultsPDF.transitionVal(ii-1) & data2plot < resultsPDF.transitionVal(ii)) = ii;
-        end
-        data2plot(data2plot > resultsPDF.transitionVal(end)) = str2num(get(gui.handles.value_PhNumHist_GUI, 'String'));
+    catch
     end
     if strcmp(get(gui.handles.binarization_GUI, 'String'), 'BINARIZATION')
         if get(gui.handles.cb_plotSectMap_GUI, 'Value')
